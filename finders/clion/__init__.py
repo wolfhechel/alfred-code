c7ce0efa0c6253c02d8d20a28f677c0c5b195462
Pontus Carlsson <pontus@thefarm.se>
2014-10-26 07:37:23 +0100

    Added finders for CLion and PhpStorm

---
+++ finders/clion/__init__.py
@@ -0,0 +1,7 @@
+from ..idea_base import IdeaBaseFinder
+
+class CLionFinder(IdeaBaseFinder):
+
+    application_id = 'com.jetbrains.CLion'
+
+    preferences_folder = 'clion10'
---
+++ finders/phpstorm/__init__.py
@@ -0,0 +1,7 @@
+from ..idea_base import IdeaBaseFinder
+
+class PhpStormFinder(IdeaBaseFinder):
+
+    application_id = 'com.jetbrains.PhpStorm'
+
+    preferences_folder = 'WebIde70'
