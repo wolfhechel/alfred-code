a47098c1a6a65ff9764537481ac445a105aaa02a
Pontus Carlsson <pontus@thefarm.se>
2014-10-28 12:25:56 +0100

    Added ability to ignore projects with fn+Enter

--- code.py
+++ code.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python2.7
 import workflow
 
+def get_ignored_items(wf):
+    return wf.stored_data('ignored_items') or []
+
+def set_ignored_items(wf, ignored_items):
+    wf.store_data('ignored_items', ignored_items)
+
 def query(finders, query, wf):
     items = []
 
+    ignored_items = get_ignored_items(wf)
+
     for finder in finders:
-        items.extend(finder.find_items())
+        items.extend([d for d in finder.find_items() if d['arg'] not in ignored_items])
 
     if query:
         items = wf.filter(query, items, lambda i: i['title'])
@@ -28,7 +36,15 @@ def open(finders, query, wf):
     if finder:
         finder.open(project_path)
 
-    return False
+def ignore(finders, query, wf):
+    escaped_query = query.replace('\\', '')
+
+    ignored_items = get_ignored_items(wf)
+
+    if escaped_query not in ignored_items:
+        ignored_items.append(escaped_query)
+
+    set_ignored_items(wf, ignored_items)
 
 if __name__ == '__main__':
     import argparse
@@ -44,6 +60,9 @@ if __name__ == '__main__':
     action.add_argument('--open', dest='action', action='store_const',
                         const=open)
 
+    action.add_argument('--ignore', dest='action', action='store_const',
+                        const=ignore)
+
     parser.add_argument('query', nargs='?', default='', type=unicode)
 
     args = parser.parse_args()
@@ -53,7 +72,11 @@ if __name__ == '__main__':
 
         available_finders = finders.get_available_finders(wf)
 
-        if args.action(available_finders, args.query, wf):
+        feedback = args.action(available_finders, args.query, wf)
+
+        if isinstance(feedback, basestring):
+            print feedback
+        elif feedback:
             wf.send_feedback()
 
     wf = workflow.Workflow()
